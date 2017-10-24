2562cf1e14cc3e59182bf353ad922c3054ef3ac3
Cole <coletschroeder@gmail.com>
2017-10-24 13:29:14 -0700

    package class

---
+++ app/src/main/java/com/example/cole/bloc3/Package.java
@@ -0,0 +1,40 @@
+package com.example.cole.bloc3;
+
+/**
+ * Created by Cole on 10/24/17.
+ */
+
+public class Package {
+    private String title, description;
+    private int imageID;
+
+    public Package(String title, String description, int imageID) {
+        this.title = title;
+        this.description = description;
+        this.imageID = imageID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+}
