cdc99575329dccb1f910352555416224376d18bc
Cole <coletschroeder@gmail.com>
2017-11-03 12:56:28 -0700

    package fragments

---
+++ app/src/main/java/com/example/cole/BLOC/FragmentMega.java
@@ -0,0 +1,30 @@
+package com.example.cole.BLOC;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Cole on 11/3/17.
+ */
+
+public class FragmentMega extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.mega_fragment, container, false);
+
+        //wire any widgets -- must use rootView.findViewById
+
+        //get any other initial set up done
+
+        //return the view that we inflated
+        return rootView;
+    }
+
+}
--- app/src/main/java/com/example/cole/BLOC/FragmentProducts.java
+++ app/src/main/java/com/example/cole/BLOC/FragmentProducts.java
@@ -3,6 +3,7 @@ package com.example.cole.BLOC;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +19,8 @@ import java.util.ArrayList;
 
 public class FragmentProducts extends Fragment {
     private ListView packageListView;
-    private ArrayList<Package> packages;
+    private ArrayList<String> packages;
+    private Fragment currentFragment;
 
     @Nullable
     @Override
@@ -32,6 +34,7 @@ public class FragmentProducts extends Fragment {
         //get any other initial set up done
         createPackages();
         setUpArrayAdapter();
+        currentFragment = new FragmentProducts();
 
         //return the view that we inflated
         return rootView;
@@ -39,15 +42,19 @@ public class FragmentProducts extends Fragment {
 
     private void createPackages() {
         packages = new ArrayList<>();
-        packages.add(new Package(getString(R.string.small_package_title), "15 Blocs, 10 Toppers", 150, 0));
-        packages.add(new Package(getString(R.string.medium_package_title), "30 Blocs, 10 Squares, 15 Toppers", 350, 0));
-        packages.add(new Package(getString(R.string.large_package_title), "50 Blocs, 30 Squares, 25 Toppers", 650, 0));
-        packages.add(new Package(getString(R.string.mega_package_title), "150 Blocs, 50 Squares, 40 Toppers", 1200, 0));
+        packages.add(getString(R.string.small_package_title));
+        packages.add(getString(R.string.medium_package_title));
+        packages.add(getString(R.string.large_package_title));
+        packages.add(getString(R.string.mega_package_title));
+        //packages.add(new Package(getString(R.string.small_package_title), "15 Blocs, 10 Toppers", 150, 0));
+        //packages.add(new Package(getString(R.string.medium_package_title), "30 Blocs, 10 Squares, 15 Toppers", 350, 0));
+        //packages.add(new Package(getString(R.string.large_package_title), "50 Blocs, 30 Squares, 25 Toppers", 650, 0));
+        //packages.add(new Package(getString(R.string.mega_package_title), "150 Blocs, 50 Squares, 40 Toppers", 1200, 0));
     }
 
     private void setUpArrayAdapter() {
-        ArrayAdapter<Package> adapter
-                = new ArrayAdapter<Package>(getActivity(), //context
+        ArrayAdapter<String> adapter
+                = new ArrayAdapter<String>(getActivity(), //context
                 R.layout.list_item_product,   //textView layout for the l
                 packages); //the list to d
         //set the adapter to the listView
@@ -55,13 +62,36 @@ public class FragmentProducts extends Fragment {
         packageListView.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) { //int i used to be pos (position in list)
+                switch (packages.indexOf(packages.get(pos))){
+                    case 0:
+                        currentFragment = new FragmentSmall();
+                        switchToNewScreen();
+                        break;
+                    case 1:
+                        currentFragment = new FragmentMedium();
+                        switchToNewScreen();
+                        break;
+                    case 2:
+                        currentFragment = new FragmentLarge();
+                        switchToNewScreen();
+                        break;
+                    case 3:
+                        currentFragment = new FragmentMega();
+                        switchToNewScreen();
+                        break;
+                }
 
-                //Intent i = new Intent(MainActivity.this, StarDescriptionActivity.class);
-
-                //i.putExtra(STAR, stars.get(pos));
-
-                //startActivity(i);
             }
         });
+    }
+
+    private void switchToNewScreen() {
+        //tell the fragment manager that if our current fragment isn't null, to replace whatever is there with it
+        FragmentManager fm = getFragmentManager();
+        if (currentFragment != null) {
+            fm.beginTransaction()
+                    .replace(R.id.fragment_container, currentFragment)
+                    .commit();
+        }
     }
 }
