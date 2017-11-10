e5d4e80d6bc21c442b25c581009175a33a1f1386
Cole <coletschroeder@gmail.com>
2017-11-09 18:19:13 -0800

    product page redone

--- app/src/main/java/com/example/cole/BLOC/FragmentProducts.java
+++ app/src/main/java/com/example/cole/BLOC/FragmentProducts.java
@@ -7,19 +7,12 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 /**
  * Created by Cole on 10/16/17.
  */
 
 public class FragmentProducts extends Fragment {
-    private ListView packageListView;
-    private ArrayList<String> packages;
     private Fragment currentFragment;
 
     @Nullable
@@ -29,60 +22,13 @@ public class FragmentProducts extends Fragment {
         View rootView = inflater.inflate(R.layout.products_fragment, container, false);
 
         //wire any widgets -- must use rootView.findViewById
-        packageListView = (ListView) rootView.findViewById(R.id.product_list_view);
 
         //get any other initial set up done
-        createPackages();
-        setUpArrayAdapter();
+
         currentFragment = new FragmentProducts();
 
         //return the view that we inflated
         return rootView;
-    }
-
-    private void createPackages() {
-        packages = new ArrayList<>();
-        packages.add(getString(R.string.small_package_title));
-        packages.add(getString(R.string.medium_package_title));
-        packages.add(getString(R.string.large_package_title));
-        packages.add(getString(R.string.mega_package_title));
-        //packages.add(new Package(getString(R.string.small_package_title), "15 Blocs, 10 Toppers", 150, 0));
-        //packages.add(new Package(getString(R.string.medium_package_title), "30 Blocs, 10 Squares, 15 Toppers", 350, 0));
-        //packages.add(new Package(getString(R.string.large_package_title), "50 Blocs, 30 Squares, 25 Toppers", 650, 0));
-        //packages.add(new Package(getString(R.string.mega_package_title), "150 Blocs, 50 Squares, 40 Toppers", 1200, 0));
-    }
-
-    private void setUpArrayAdapter() {
-        ArrayAdapter<String> adapter
-                = new ArrayAdapter<String>(getActivity(), //context
-                R.layout.list_item_product,   //textView layout for the l
-                packages); //the list to d
-        //set the adapter to the listView
-        packageListView.setAdapter(adapter);
-        packageListView.setOnItemClickListener(new ListView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) { //int i used to be pos (position in list)
-                switch (packages.indexOf(packages.get(pos))){
-                    case 0:
-                        currentFragment = new FragmentSmall();
-                        switchToNewScreen();
-                        break;
-                    case 1:
-                        currentFragment = new FragmentMedium();
-                        switchToNewScreen();
-                        break;
-                    case 2:
-                        currentFragment = new FragmentLarge();
-                        switchToNewScreen();
-                        break;
-                    case 3:
-                        currentFragment = new FragmentMega();
-                        switchToNewScreen();
-                        break;
-                }
-
-            }
-        });
     }
 
     private void switchToNewScreen() {
