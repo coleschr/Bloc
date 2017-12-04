df7c7d27e4cfb1597afe1e463bbc650342dc9439
Cole <coletschroeder@gmail.com>
2017-12-04 14:10:05 -0800

    games

--- app/src/main/java/com/example/cole/BLOC/Games/FragmentStacker.java
+++ app/src/main/java/com/example/cole/BLOC/Games/FragmentStacker.java
@@ -1,4 +1,4 @@
-package com.example.cole.BLOC;
+package com.example.cole.BLOC.Games;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,58 +7,54 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 
-import com.example.cole.BLOC.Games.FragmentSliding;
-import com.example.cole.BLOC.Games.FragmentStacker;
+import com.example.cole.BLOC.FragmentExplore;
+import com.example.cole.BLOC.R;
 
 /**
- * Created by Cole on 10/16/17.
+ * Created by Cole on 12/4/17.
  */
 
-public class FragmentExplore extends Fragment implements View.OnClickListener{
-    private Button stacker, sliding;
+public class FragmentStacker extends Fragment implements View.OnClickListener{
+    private ImageView back;
     private Fragment currentFragment;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.explore_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.stacker_fragment, container, false);
 
         //wire any widgets -- must use rootView.findViewById
-        stacker = (Button) rootView.findViewById(R.id.button_stacker);
-        sliding = (Button) rootView.findViewById(R.id.button_sliding);
+        wireWidgets(rootView);
 
         //get any other initial set up done
         setOnClickListeners();
-        currentFragment = new FragmentExplore();
 
         //return the view that we inflated
         return rootView;
     }
 
+    private void wireWidgets(View rootView) {
+        back = (ImageView) rootView.findViewById(R.id.imageView_stacker_back);
+    }
+
     private void setOnClickListeners() {
-        stacker.setOnClickListener(this);
-        sliding.setOnClickListener(this);
+        back.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.button_stacker:
-                currentFragment = new FragmentStacker();
-                switchToNewScreen();
-                break;
-            case R.id.button_sliding:
-                currentFragment = new FragmentSliding();
-                switchToNewScreen();
+        switch(view.getId()){
+            case R.id.imageView_stacker_back:
+                currentFragment = new FragmentExplore();
+                switchToNewScreen(currentFragment);
                 break;
         }
-
     }
 
-    private void switchToNewScreen() {
+    private void switchToNewScreen(Fragment currentFragment) {
         //tell the fragment manager that if our current fragment isn't null, to replace whatever is there with it
         FragmentManager fm = getFragmentManager();
         if (currentFragment != null) {
